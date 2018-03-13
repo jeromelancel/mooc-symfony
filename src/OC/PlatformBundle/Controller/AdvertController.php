0466ab400e21d7722c2606258cfc583660b2d717
Jérôme LANCEL <jerome.lancel@outlook.fr>
2018-03-13 19:48:39 +0100

    Ajout d'une route et d'une méthode au controleur advert

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -13,9 +13,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller{
+
+
+    public function viewAction($id)
+    {
+        return new Response("Affichage de l'annonce d'id : ".$id);
+    }
+
+    // ... et la méthode indexAction que nous avons déjà créée
     public function indexAction()
     {
         $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Jérôme'));
         return new Response($content);
+    }
+
+    public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
     }
 }
